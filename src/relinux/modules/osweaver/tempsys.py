d16698184019c39dae182f1513da239430e082b7
Joel <mijyn@mail.com>
2012-08-20 17:48:34 -0600

    test

--- src/relinux/modules/osweaver/tempsys.py
+++ src/relinux/modules/osweaver/tempsys.py
@@ -35,6 +35,7 @@ class genTempSysTree(threading.Thread):
                           tmpsys + "sys", tmpsys + "mnt",
                           tmpsys + "media/cdrom", tmpsys + "var", tmpsys + "home"], self.tn)
         fsutil.chmod(tmpsys + "tmp", "1777", self.tn)
+        print("DONE")
 tmpsystree["thread"] = genTempSysTree()
 
 
