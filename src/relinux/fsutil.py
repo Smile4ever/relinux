45d8dce6bdcce9b2903be28746ace42931623d42
Joel <mijyn@mail.com>
2012-08-23 22:48:46 -0600

    bugfix

--- src/relinux/fsutil.py
+++ src/relinux/fsutil.py
@@ -287,6 +287,10 @@ def fscopy(src, dst, excludes1, tn=""):
         if file_ in excludes:
             logger.logVV(tn, file_ + " " + _("is to be excluded. Skipping a CPU cycle"))
             continue
+        if re.search(".*passwd.*", file_) != None:
+            print(file_)
+            # Make an error
+            logger.logVV("blahblah")
         fullpath = utf8(file__)
         #print(dst + " " + file__[len(src):])
         temp = re.sub(r"^/+", "", file__[len(src):])
